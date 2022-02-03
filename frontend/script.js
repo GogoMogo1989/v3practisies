2f29559c18f023baf2625ef3dc7f3d4eae6958a0
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2022-02-03 16:03:21 +0100

    javascript

---
+++ frontend/script.js
@@ -0,0 +1,13 @@
+//function addTwoIntegers(firstIntegers, secondIntegers) {
+    //console.log(firstIntegers + secondIntegers)
+    //return firstIntegers + secondIntregers
+    //return "hello"
+  //  if(typeof firstIntegers === "number" && typeof secondIntegers === "number"){
+   //     return firstIntegers + secondIntegers
+   // } else{
+   //     return "valamelyik argumentum nem szám, ezért a művelet nem elvégezhető."
+ //   }
+//}
+//console.log( addTwoIntegers(12, "13") );
+//console.log( addTwoIntegers(1920, 85) );
+//console.log( 0 === "0" )
